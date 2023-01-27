551dc89bd914da630d96d1ec5456495bfa7b375b
Jingjie He <jingjie8@illinois.edu>
2023-01-27 14:06:26 -0600

    good

--- auto_driving.py
+++ auto_driving.py
@@ -7,10 +7,15 @@ def main():
         if not scan_list:
             continue
             
-        tmp = scan_list[3:7]
-        print(tmp)
-        print(scan_list)
-
+        if scan_list[3:7] != [2,2,2,2]:
+            if(scan_list[0] or scan_list[1] or scan_list[2]):
+                fc.turn_left(10)
+            elif(scan_list[7] or scan_list[8] or scan_list[9]):
+                fc.turn_right(10)
+            else:
+                fc.backward(5)
+        else:
+            fc.forward(20)
 if __name__ == "__main__":
     try:
         main()

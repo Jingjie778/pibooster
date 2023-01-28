0cd601813b928dc328b980e923986832bf9506b7
Jingjie He <jingjie8@illinois.edu>
2023-01-28 11:02:06 -0600

    good

--- auto_driving.py
+++ auto_driving.py
@@ -1,22 +1,17 @@
 import picar_4wd as fc
 import random
-power = 20
 
 def main():
     while True:
         scan_list = fc.scan_step(30)
         if not scan_list:
             continue
-            
-        left_sum =scan_list[0] + scan_list[1] + scan_list[2]
-        right_sum =scan_list[7] + scan_list[8] + scan_list[9]
+        det = random.randint(0,1)
         if scan_list[3:7] != [2,2,2,2]:
-            if(left_sum > right_sum):
+            if(det):
                 fc.turn_left(10)
-            elif(left_sum < right_sum):
-                fc.turn_right(10)
             else:
-                fc.backward(5)
+                fc.turn_right(10)
         else:
             fc.forward(20)
             
